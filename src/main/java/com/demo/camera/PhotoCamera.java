71502b1e7976ee96c8b3817995307a9e3bb0bcbf
Michal Dysput <michal.dysput@gmail.com>
2018-03-20 19:39:58 +0100

    4

--- src/main/java/com/demo/camera/PhotoCamera.java
+++ src/main/java/com/demo/camera/PhotoCamera.java
@@ -1,17 +1,50 @@
 package com.demo.camera;
 
-public class PhotoCamera {
+public class PhotoCamera implements WriteListener {
 
-    public void turnOn() {
-        // not implemented
+    private boolean isOn = false;
+    private final ImageSensor sensor;
+    private final Card card;
+
+    public PhotoCamera(ImageSensor sensor, Card card) {
+        this.sensor= sensor;
+        this.card = card;
     }
 
-    public void turnOff() {
-        // not implemented
+
+    public boolean isOn(){
+        return isOn;
+    }
+
+    private void turnOn() {
+        isOn = true;
+        sensor.turnOn();
+    }
+
+    private void turnOff() {
+        isOn = false;
+        sensor.turnOff();
     }
 
     public void pressButton() {
-        // not implemented
+        if(isOn){
+            //sensor.read();
+            card.write(sensor.read());
+        }
+    }
+
+    public void powerButton(){
+        if(isOn){
+            turnOff();
+        }
+        else{
+            turnOn();
+        }
+    }
+
+    @Override
+    public void writeCompleted() {
+
     }
 }
 
--- src/test/java/com/demo/camera/PhotoCameraTest.java
+++ src/test/java/com/demo/camera/PhotoCameraTest.java
@@ -2,13 +2,95 @@ package com.demo.camera;
 
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 
 public class PhotoCameraTest {
 
 
     @Test
-    public void changeName() {
-        Assertions.assertThat(this).isNotNull();
+    public void cameraIsOffAfterCreating(){
+        Card card= mock(Card.class);
+        ImageSensor sensor = mock(ImageSensor.class);
+        PhotoCamera camera = new PhotoCamera(sensor, card);
+        Assertions.assertThat(camera.isOn()).isFalse();
     }
+
+    @Test
+    public void afterPressingPowerButtonCameraWillBeOnIfItWasOff(){
+        ImageSensor sensor = mock(ImageSensor.class);
+        Card card= mock(Card.class);
+        PhotoCamera camera = new PhotoCamera(sensor, card);
+        assert !camera.isOn();
+        camera.powerButton();
+        Assertions.assertThat(camera.isOn()).isTrue();
+    }
+
+    @Test
+    public void afterPressingPowerButtonCameraWillBeOnIfItWasOffAndSensorWillBeAlsoOn(){
+        ImageSensor sensor = mock(ImageSensor.class);
+        Card card= mock(Card.class);
+        PhotoCamera camera = new PhotoCamera(sensor, card);
+        assert !camera.isOn();
+        camera.powerButton();
+        Mockito.verify(sensor, times(1)).turnOn();
+    }
+    @Test
+    public void afterPressingPowerButtonCameraWillBeOffIfItWasOnAndSensorWillBeAlsoOff(){
+        ImageSensor sensor = mock(ImageSensor.class);
+        Card card= mock(Card.class);
+        PhotoCamera camera = new PhotoCamera(sensor, card);
+        camera.powerButton();
+        assert camera.isOn();
+        camera.powerButton();
+        Mockito.verify(sensor, times(1)).turnOff();
+    }
+
+    @Test
+    public void pressingTriggeringButtonIfPowerIsOffDoNothing(){
+        ImageSensor sensor = mock(ImageSensor.class);
+        Card card= mock(Card.class);
+        PhotoCamera camera = new PhotoCamera(sensor, card);
+        camera.pressButton();
+        Mockito.verify(sensor, times(0)).read();
+    }
+    @Test
+    public void pressingTriggeringButtonIfPowerIsOnCopyDataFromSensor(){
+        ImageSensor sensor = mock(ImageSensor.class);
+        Card card= mock(Card.class);
+        PhotoCamera camera = new PhotoCamera(sensor, card);
+        camera.powerButton();
+        camera.pressButton();
+        Mockito.verify(sensor, times(1)).read();
+    }
+     @Test
+    public void pressingTriggeringButtonIfPowerIsOnCopyDataFromSensorToCard(){
+        ImageSensor sensor = mock(ImageSensor.class);
+        Card card= mock(Card.class);
+        PhotoCamera camera = new PhotoCamera(sensor, card);
+        camera.powerButton();
+        camera.pressButton();
+        Mockito.verify(sensor, times(1)).read();
+        Mockito.verify(card, times(1)).write(Mockito.any());
+    }
+      @Test
+    public void pressingTriggeringButtonIfPowerIsOnCopyDataFromSensorToCardAndSaveInFewSeconds(){
+        ImageSensor sensor = mock(ImageSensor.class);
+        Card card= mock(Card.class);
+        PhotoCamera camera = new PhotoCamera(sensor, card);
+        camera.powerButton();
+        camera.pressButton();
+        Mockito.verify(sensor, times(1)).read();
+        Mockito.verify(card, times(1)).write(Mockito.any());
+    }
+
+
+
+
+
+
+
+
 
 }
